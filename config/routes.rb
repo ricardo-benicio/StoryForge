985e38c223cda413e9ac0cc9cda1d998660b5395
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-07-17 16:18:11 -0300

    refactor: create routes dashboard and author

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'dashboard#index'
+  resources :authors
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 end
