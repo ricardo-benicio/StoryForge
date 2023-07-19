66e0b09c07bcc25b6982a8c8676436c46ced8845
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-07-19 16:27:12 -0300

    refactor: create route authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'dashboard#index'
   resources :authors
+  root 'dashboard#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
