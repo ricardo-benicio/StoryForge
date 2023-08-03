3104ac2a139dbef5241c6853bce35c3ace43d750
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-08-03 16:42:06 -0300

    refactor: correct validation cpf

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,11 @@
+require 'cpf_cnpj'
 class Author < ApplicationRecord
   validates :name, presence: true, length: {minimum: 3, maximum: 50}
   validates :cpf, presence: true, uniqueness: true
-  before_validation :cpf_is_valid
+  before_validation :cpf_is_valid?
 
   private
-  def cpf_is_valid
-    errors.add(:cpf, 'invalido!') unless CPF::VerifierDigit.tap
-    #self.cpf = CPF.valid?(cpf, strict: false) if cpf.present?
+  def cpf_is_valid?
+    self.cpf = CPF.new(cpf, strict: false).formatted if cpf.present?
   end
 end
