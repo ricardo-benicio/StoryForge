81090c26d5ad90a72cf6ea2c788683e4ca7db54f
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-08-07 18:05:17 -0300

    feat: add api controller authors

---
+++ app/controllers/api/authors_controller.rb
@@ -0,0 +1,51 @@
+module Api
+  class AuthorsController < ApplicationController
+    #skip_before_action :valid_authenticity_token, only: %i[ create update destroy ]
+    before_action :set_author, only: %i[ show update destroy ]
+
+    def index
+      @author = Author.all
+    end
+
+    def show; end
+
+    def create
+      @author = Author.new(author_params)
+
+      if @author.save
+        render :create, status: :created
+      else
+        render json: {errors: @author.errors.full_messages}, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @author.update(author_params)
+        render :update, status: :ok
+      else
+        render json: {errors: @author.errors.full_messages}, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      if @author.destroy
+        render json: {message: "Author deleted successfully"}, status: :ok
+      else
+        render json: {errors: @author.errors.full_messages}, status: :unprocessable_entity
+      end
+    end
+
+    private
+    def set_author
+      @author = Author.find(params[:id])
+
+      render @author
+
+      render json: {message: "Author not found."}, status: :not_found
+    end
+
+    def author_params
+      params.require(:author).permit(:name)
+    end
+  end
+end
