902a0b5f1799a42260c61595326455d4e2b33510
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-08-11 17:37:06 -0300

    feat: test author request.

---
+++ spec/requests/api/authors_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "Api::Authors", type: :request do
+  describe "GET /api/authors" do
+    let!(:author) {create_list(:author, 3)}
+
+    it 'returns all authors' do
+      get api_authors_path
+    end
+
+    it 'returns status code 200' do
+      get api_authors_path
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST /api/authors" do
+    context "creating a new author" do
+      let(:valid_author_params) do
+        {
+          author: {
+            name: "New Author",
+            cpf: "000.000.000-00"
+          }
+        }
+      end
+
+      it 'create author' do
+        expect do
+          post api_authors_path, params: valid_author_params
+        end.to change(Author, :count).by(0)
+        expect(response).to have_http_status(:created)
+        #expect(json["name"]).to eq("New Author")
+        #expect(json["cpf"]).to eq("000.000.000-00")
+
+      end
+    end
+  end
+end
