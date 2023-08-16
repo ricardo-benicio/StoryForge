1709ab8492ceb4f97df6e39ee8daec2cf2283deb
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-08-16 15:12:15 -0300

    test: implement test authors

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:author) { create(:author) }
+
+  describe 'Present name Author' do
+    context 'When name is prensent' do
+      it('Author create') { expect(author).to be_valid }
+    end
+  end
 end
