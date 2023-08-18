7e5ec37ae7f2730571451e90b2f39564d4fe5fd6
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-08-18 16:45:04 -0300

    feat: add column title to table books.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_174606) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_170854) do
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
     t.datetime "created_at", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_174606) do
     t.datetime "updated_at", null: false
     t.integer "author_id", null: false
     t.string "isbn"
+    t.string "title"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
