baf97f9a9c4d4beb964860a3661c29eac775986b
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-08-18 16:36:38 -0300

    test: implement tests model book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,28 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe Book, type: :model do
+RSpec.describe Book do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    describe 'title' do
+      it 'must be present' do
+        book = FactoryBot.create(:book)
+        expect(book).to_not be_valid
+        book.title = 'teste do testando'
+        expect(book).to be_valid
+      end
+    end
+
+    describe 'isbn' do
+      it 'must be present' do
+        book = FactoryBot.create(:book)
+        expect(book).to_not be_valid
+        isbn_teste = FFaker::Book.isbn
+        book.isbn = isbn_teste
+        expect(book).to be_valid
+      end
+    end
+  end
 end
