f93ec4491a5f64810e613ebc853fa02c71046337
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-22 15:14:28 -0300

    refactor: change module test

--- spec/helpers/api/author_helper_spec.rb
+++ spec/helpers/api/author_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe ApiController::AuthorHelper, type: :helper do
+RSpec.describe Api::AuthorHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
