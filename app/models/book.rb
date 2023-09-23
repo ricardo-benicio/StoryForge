f8bd07b91ce7615478054227eb31d120dd320f84
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-23 15:29:53 -0300

    refactor: change method and validates.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,12 +1,15 @@
+# frozen_string_literal: true
+
 require 'isbn'
 class Book < ApplicationRecord
   belongs_to :author
 
-  validates :isbn, presence: true, uniqueness: true
+  validates :isbn, presence: false
   before_validation :isbn_is_valid?
 
   private
+
   def isbn_is_valid?
-    self.isbn = ISBN.as_new(isbn) if isbn.present?
+    self.isbn = ISBN.thirteen(isbn) if isbn.present?
   end
 end
